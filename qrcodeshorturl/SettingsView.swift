89191c30bc60293017123f9aedc6f3dec387f455
Richard Waithe <richardm1@Richards-MacBook-Pro-2.local>
2024-11-06 15:33:36 -0500

    updated build

--- qrcodeshorturl/SettingsView.swift
+++ qrcodeshorturl/SettingsView.swift
@@ -27,7 +27,7 @@ struct SettingsView: View {
                     HStack {
                         Text("Version")
                         Spacer()
-                        Text("1.4.1")
+                        Text("1.4.3")
                             .foregroundColor(.gray)
                     }
                     
